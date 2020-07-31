276283a9246f0e0ec0d2bca42276c2b1477ac3db
eileeny <eileen.k.ye@gmail.com>
2020-07-31 11:56:47 -0400

    uses pandas and calculates max

--- fixedWeights.py
+++ fixedWeights.py
@@ -1,33 +1,39 @@
-import csv
+import numpy as num
+import pandas as pd
 
-# 0: senior center, 1: elderly, 2: kids under 5, 3: cooling center, 4: caregiver organization, 5: 100% poverty
-# 6: 100%-150% poverty, 7: 150% poverty, 8: affordable housing building, 9: affordable housing unit
-# could also add max values to the csv file and read in from there
-maxvalues = [8, 29.07, 14.00, 6, 2, 35.6, 38.5, 71, 160, 15396]
+df = pd.read_csv('Data by Zipcode - Sheet1.csv')
+
+maxvalues = {}
+for label, column in df.items(): # initialize maxvalues to all 0s
+	if label == "Zip_Codes":
+		continue
+	else:
+		maxvalues[label] = 0
 
 fin = open ('test.in', 'r')
 fout = open('test.out', 'w')
 
 zipcode = fin.readline().strip()
-zipdata = [] # data for the zipcode from the csv file
+zipdata = {} # data for the zipcode from the csv file
 
-with open('Data by Zipcode - Sheet1.csv') as csvfile:
-	readCSV = csv.reader(csvfile, delimiter=',')
-	for row in readCSV:
-		if (row[0] == zipcode):
-			for i in range(1, 11):
-				zipdata.append(row[i])
+for ind in df.index:
+	correctrow = False
+	if int(df["Zip_Codes"][ind]) == int(zipcode): #the correct row
+		correctrow = True
+	for label, column in df.items(): #update maxvalues
+		if label == "Zip_Codes": continue
+		else:
+			if df.loc[ind, label] > maxvalues[label]:
+				maxvalues[label] = df[label][ind]
+			if correctrow:
+				zipdata[label] = df[label][ind]
 
 R = 0
 W = 1/10 # 10 vulnerability categories in the dataset
 
-for i in range(10):
-	if zipdata[i][-1] == '%':
-		zipdata[i] = zipdata[i][:-1]
-	R += (float(zipdata[i])/maxvalues[i])*W
+for key in zipdata:
+	R += (float(zipdata[key])/maxvalues[key])*W
 
 fout.write (str(R))
 fout.close()
-
-
 
